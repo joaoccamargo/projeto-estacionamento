c6cf897d438226326f00ec4f149c2a2ede09f508
João Camargo <jclaudio00_12@hotmail.com>
2023-05-09 11:31:15 -0300

    adjusted controllers

--- frontend/controller/lista-Clientes.js
+++ frontend/controller/lista-Clientes.js
@@ -1,6 +1,10 @@
 import { view } from "../view/index.js";
+import { service } from "../service/index.js"
 
 export const ListaClienteComponent = () => {
     view.getListaClientesHtml();
 
+    service.getVeiculo().then((dados) => {
+        console.log(dados)
+    })
 }
