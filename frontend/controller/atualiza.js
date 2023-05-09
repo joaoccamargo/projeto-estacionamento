c1607955de7e44ca1b0b035354485c2b2ebb5419
João Camargo <jclaudio00_12@hotmail.com>
2023-05-09 16:02:19 -0300

    future review

--- frontend/controller/atualiza.js
+++ frontend/controller/atualiza.js
@@ -1,5 +1,6 @@
 import { service } from "../service/index.js";
 import { view } from "../view/index.js";
+import { ListaClienteComponent } from "./lista-Clientes.js";
 
 export const AtualizaComponent = (idParametro) => {
     view.getAtualizaHtml();
@@ -12,6 +13,24 @@ export const AtualizaComponent = (idParametro) => {
             }
         });
     })
+
+    const formulario = document.getElementById('formulario')
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const atualizaCliente = {
+            owner: document.getElementById('name').value,
+            model: document.getElementById('modelo').value,
+            type: document.getElementById('tipo').value,
+            label: document.getElementById('placa').value,
+            observation: document.getElementById('observacoes').value
+        }
+        // Revisão Futura
+        service.putVeiculo(atualizaCliente, idParametro).then(() => {
+            cancelar();
+            ListaClienteComponent();
+        })
+
+     })
 }
 
 const adicionaParametroNoInput = (objeto) => {
@@ -20,4 +39,10 @@ const adicionaParametroNoInput = (objeto) => {
         document.getElementById('tipo').value = objeto.type
         document.getElementById('placa').value = objeto.label
         document.getElementById('observacoes').value = objeto.observation
+}
+
+const cancelar = () => {
+   const formulario = document.getElementById("formulario")
+   formulario.reset();
+
 }
