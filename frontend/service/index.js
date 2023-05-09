23bf3ae66555313cf4cce38f74bbc21c104f7099
João Camargo <jclaudio00_12@hotmail.com>
2023-05-09 11:29:40 -0300

    service adjusted

--- frontend/service/index.js
+++ frontend/service/index.js
@@ -1,12 +1,37 @@
 const url = "http://localhost:8000/api";
 
-export const postVeiculo = (objetoCliente) => {
-    console.log("objetoCliente: ", JSON.stringify(objetoCliente))
-    fetch(url + "/vehicles", {
+// -----------------POST-----------------------
+const postVeiculo = (objetoCliente) => {
+    //console.log("objetoCliente: ", JSON.stringify(objetoCliente))
+    return fetch(url + "/vehicles", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(objetoCliente)
+    }).then((response) => {
+        if(response.status != 200){
+            console.log(`Erro no servidor: ${response.status}`)
+        }else{
+            alert(`Sucesso ao salvar: ${response.status}`)
+        }
     })
+}
+
+// -----------------GET-----------------------
+const getVeiculo = () => {
+    return fetch(url + "/vehicles",{
+        method: "GET"
+    }).then((response) => {
+        if(response.status != 200){
+            console.log(`Erro no servidor: ${response.status}`)
+        }else{
+            return response.json()
+        }
+    })
+}
+
+export const service = {
+    postVeiculo,
+    getVeiculo
 }
